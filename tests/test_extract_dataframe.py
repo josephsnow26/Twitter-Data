dbbd1d7745d5a3471de434f97eda27131de23ae3
joseph <57895391+josephsnow26@users.noreply.github.com>
2022-08-09 15:58:48 +0100

    Update test_extract_dataframe.py

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -34,8 +34,8 @@ import sys, os
 
 sys.path.append(os.path.abspath(os.path.join("../..")))
 
-from Untitled import read_json
-from Untitled import TweetDfExtractor
+from extract_dataframe import read_json
+from extract_dataframe import TweetDfExtractor
 
 # For unit testing the data reading and processing codes, 
 # we will need about 5 tweet samples. 
